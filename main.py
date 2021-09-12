32feb900b4fda25f8d2cba545e171d78b5073a2e
Phoenix-Lu <2653347289@qq.com>
2021-09-12 15:16:58 +0800

    9.12 请注意所有的range函数！！！！！ 完成using part

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from openpyxl import load_workbook
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 
-#全篇的sleep可以优化
-
+#全篇的sleep函数可以优化
+#打不开的情况需要try函数优化
 
 
 class Part:
@@ -22,8 +22,6 @@ class Part:
         self.part_id = part_id
         self.part_url = part_url
         self.short_desc = short_desc
-        self.part_type = part_type
-        self.team = team
         self.year = year
         self.sequence = sequence
         #stars 需要再细化
@@ -35,10 +33,13 @@ class Part:
         self.parts_used = parts_used
         self.using_parts = using_parts
         self.len = len
+        self.part_type = part_type
+        self.team = team
 
     def print_parts(self):
         print(f"part_num = {self.part_num}")
         print(f"part_name = {self.part_name}")
+
         print(f"part_id = {self.part_id}")
         print(f"part_url = {self.part_url}")
         print(f"part_type = {self.part_type}")
@@ -137,7 +138,7 @@ def get_parts_urls(all_team_with_urls):
         for item in driver.find_elements_by_xpath('/html/body/div/div[4]/div/div/table[1]/tbody/tr/td[3]/a'):
             part_num_list.append(str(item.text))
             part_numurl_list.append(item.get_attribute('href'))
-        for item in driver.find_elements_by_xpath('/html/body/div/div[4]/div/div/table[1]/tbody/tr/td[4]'):
+        for item in driver.find_elements_by_xpath('/html/body/div/div[4]/div/div``/table[1]/tbody/tr/td[4]'):
             part_type_list.append(str(item.text))
         for item in driver.find_elements_by_xpath('/html/body/div/div[4]/div/div/table[1]/tbody/tr/td[5]'):
             part_desc.append(str(item.text))
@@ -147,7 +148,7 @@ def get_parts_urls(all_team_with_urls):
             part_len.append(str(item.text))
         #为第第一张表（favored）创建类
             #star第一个1代表favor
-        for i in range(0, len(part_num_list)-1):
+        for i in range(0, len(part_num_list)):
             new_part = Part(part_num_list[i], '', '', part_numurl_list[i], part_desc[i], part_type_list[i], team, year, '', '', '1', '', [], [], [], part_len[i])
             whole_Parts.append(new_part)
 
@@ -179,48 +180,68 @@ def get_parts_urls(all_team_with_urls):
     return
 
 
-# 可能需要拆分
-
+# 从part主界面出发的 主函数
 def get_parts_details():
-    get_sequence()
-    return 0
-
-
-#used代表使用了该part的part
-def get_used_parts():
-    return
-
-
-
-
-#using代表该part的组成part
-def get_using_parts():
-    return
-
-
-def get_sequence():
     for a_part in whole_Parts:
         url = a_part.part_url
         driver = webdriver.Chrome('D:\chromedriver.exe')
         driver.get(url)
         time.sleep(5)
-        sequence_entrance = driver.find_elements_by_xpath('//*[@id="seq_features_div"]/div[1]/div[1]/span[5]')
-        #webdriver.ActionChains(driver).move_to_element(sequence_entrance[0]).click(sequence_entrance[0]).perform().find_elements_by_xpath("/html/body/pre/text()")
-        webdriver.ActionChains(driver).move_to_element(sequence_entrance[0]).click(sequence_entrance[0]).perform()
-        time.sleep(1)
-        #切换窗口到新跳出的窗口
-        handles = driver.window_handles
-        index_handle = driver.current_window_handle#备注：可能需要在操作前，先关闭其他浏览器窗口
-        for handle in handles:
-            if handle != index_handle:
-                driver.switch_to.window(handle)
-        sequence = driver.find_elements_by_xpath("/html/body/pre")#备注：所有xpath出来都是list，记得切换为元素
-        a_part.sequence = str(sequence[0].text)
-        driver.close()
+        #以上打开了part的主网页界面
+        #-------------------------------------------
+        get_using_parts_and_other_info(driver, a_part)
+        get_sequence(driver, a_part)
+    return 0
 
-        handle = driver.window_handles[0]
-        driver.switch_to.window(handle)
-        driver.close()
+
+#used代表使用了该part的part，需要额外打开页面
+def get_used_parts():
+    for a_part in whole_Parts:
+        url = a_part.part_url
+        driver = webdriver.Chrome('D:\chromedriver.exe')
+        driver.get(url)
+        time.sleep(5)
+    return
+
+
+
+
+#这一部分写part主页面内的所有内容。using代表该part的组成part,不需要额外打开页面加载
+def get_using_parts_and_other_info(driver, a_part):
+    if a_part.part_type != 'Composite':
+        a_part.using_parts = ['self']
+    else:
+        using_parts_list = []
+        for item in driver.find_elements_by_xpath('//*[@id="seq_features_div"]/div[1]/div[4]/div/div[2]'):
+            using_parts_list.append(str(item.text))
+        #以下确认了编号的统一
+        for i in range(0, len(using_parts_list)):
+            if 'BBa' in using_parts_list[i]:
+                continue
+            else:
+                using_parts_list[i] = 'BBa_'+ using_parts_list[i]
+
+        a_part.using_parts = using_parts_list
+    return
+
+
+def get_sequence(driver, a_part):
+    sequence_entrance = driver.find_elements_by_xpath('//*[@id="seq_features_div"]/div[1]/div[1]/span[5]')
+    #webdriver.ActionChains(driver).move_to_element(sequence_entrance[0]).click(sequence_entrance[0]).perform().find_elements_by_xpath("/html/body/pre/text()")
+    webdriver.ActionChains(driver).move_to_element(sequence_entrance[0]).click(sequence_entrance[0]).perform()
+    time.sleep(1)
+    #切换窗口到新跳出的窗口
+    handles = driver.window_handles
+    index_handle = driver.current_window_handle#备注：可能需要在操作前，先关闭其他浏览器窗口
+    for handle in handles:
+        if handle != index_handle:
+            driver.switch_to.window(handle)
+    sequence = driver.find_elements_by_xpath("/html/body/pre")#备注：所有xpath出来都是list，记得切换为元素
+    a_part.sequence = str(sequence[0].text)
+    driver.close()
+    handle = driver.window_handles[0]
+    driver.switch_to.window(handle)
+    driver.close()
     return
 
 
@@ -242,121 +263,15 @@ def store_parts(team_parts_list):
     print('--------parts are stored-------')
 
 
-'''
-def get_team_lists(year):
-    front_url = "https://igem.org/Team_Parts?year="
-    url = front_url + year
-    print(f"--------Now it's searching for {year}, url:{url} --------")
-    strhtlm = requests.get(url)
-    soup = BeautifulSoup(strhtlm.text, 'lxml')
-    urls = []
-    for item in soup.find_all('a'):
-        urls.append(item.get('href'))
-'''
-'''
-        result={
-            "title":item.get_text(),
-            "link":item.get('href'),
-            #'ID':re.findall('\d+',item.get('href'))
-        }
-        print(result)
-'''
-'''
-    # table_Tea ms_from_Asia > tbody > tr: > td: > div > a
-    # table_Teams_from_Asia > tbody > tr:nth-child(1) > td:nth-child(2) > div > a
-    # table_Teams_from_Asia > tbody > tr:nth-child(1) > td:nth-child(1) > div > a
-    del urls[0]
-    del urls[0]
-    print(f"lists_geted:{urls}\n{urls.count} team in all")
-    print("--------get_lists_ended--------\n")
-    return urls
 
-
-def get_parts(urls, year):
-    print(f"Now searching for parts in {year}")
-    count = 1
-    answer = []
-    for url in urls:
-        time.sleep(1)
-        print(f"Wait for a while,NO.{count}\n")
-        count = count + 1
-        print("\n")
-        team_name = url[67:]
-        strhtlm = requests.get(url)
-        soup = BeautifulSoup(strhtlm.text, 'lxml')
-        parts_list = []
-        parts_urls = []
-        for item in soup.find_all('a'):
-            parts_list.append(item.get_text())
-            parts_urls.append(item.get('href'))
-        for i in range(1, 5):
-            if (parts_list == []):
-                break
-            else:
-                del parts_list[0]
-                del parts_urls[0]
-        '''
-'''
-        while '' in parts_list:
-            list.remove('')
-
-
-
-            if (parts_list[0]==['']):
-                break
-            else:
-                del parts_list[0]
-                del parts_urls[0]
-                '''
-'''
-        this_team = Team_parts(year, team_name, parts_list, parts_urls, [], [])
-        answer.append(this_team)
-    print("--------got all parts--------")
-    return answer
-
-'''
-
-'''
-def get_detailed_parts (team_parts_lists):
-    for item in team_parts_lists:
-        detailed_url = item.part_url
-        for url in detailed_url:
-            strhtlm = requests.get(url)
-            website = etree.HTML(strhtlm.text)
-            item.part_type = website.xpath('//*[@id="content"]/div[2]/div[1]/text()')
-            print(item.part_type)
-            if (item.part_type == ['coding']):
-                item.basic_parts == [website.xpath('//*[@id="content"]/div[2]/div[2]/text()')]
-            #if (item.part_type == )
-
-        return (team_parts_lists)
-        '''
-
-"""MAIN"""
-
-'''
-def main():
-
-    # 更改此处【2020】为想要查看年份即可，这里以2020为例：
-    for year in [2019]:
-        urls = get_team_lists(str(year))
-        # urls = ['http://parts.igem.org/cgi/partsdb/pgroup.cgi?pgroup=iGEM2020&group=AFCM-Egypt']
-        team_pars_list = get_parts(urls, str(year))
-        # for item in team_pars_list:
-        #    print(f"{item.year}, {item.team_name}, {item.part_name}, {item.part_url}")
-        # print(f"all parts in {year} are printed")\
-        # output = get_detailed_parts(team_pars_list)
-        sotre_parts(team_pars_list)
-    print("All_Exit")
-
-'''
 
 
 def main():
-    year = '2020'
+    year = '2004'
     # for year in range(2020):
-    ##all_team_with_urls = web_analysis_and_get_team_lists(str(year))
-    all_team_with_urls = [['2020','teamA','http://parts.igem.org/cgi/partsdb/pgroup.cgi?pgroup=iGEM2020&group=GDSYZX'],['2020','teamB',' http://parts.igem.org/cgi/partsdb/pgroup.cgi?pgroup=iGEM2020&group=Fudan']]
+    #all_team_with_urls = web_analysis_and_get_team_lists(str(year))
+    #anothoer test_example:  ['2020','teamB',' http://parts.igem.org/cgi/partsdb/pgroup.cgi?pgroup=iGEM2020&group=Fudan']
+    all_team_with_urls = [['2020', 'teamA', 'http://parts.igem.org/cgi/partsdb/pgroup.cgi?pgroup=iGEM2020&group=GDSYZX']]
     get_parts_urls(all_team_with_urls)
     get_parts_details()
     for item in whole_Parts:
